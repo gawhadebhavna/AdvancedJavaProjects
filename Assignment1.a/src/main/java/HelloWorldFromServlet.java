baf464d870a81f205171cdffc7acf6ca2c3cc233
bhavna <bhavnagawhade11.com>
2021-12-31 15:37:04 +0530

    HelloWorldFromServlet has completed

---
+++ Assignment1.a/src/main/java/HelloWorldFromServlet.java
@@ -0,0 +1,13 @@
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+public class HelloWorldFromServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        out.println("<h3>Hello World From Servlet!</h3>");
+    }
+}
